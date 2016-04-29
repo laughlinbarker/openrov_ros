4522de705169ff4fa79a8bba106a328565ce2ad6
Laughlin Barker <laughlin@jhu.edu>
2016-04-28 21:52:40 -0400

    added Eigen Libs, compiles cleanly

--- teleop/src/teleop.cpp
+++ teleop/src/teleop.cpp
@@ -8,7 +8,11 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Wrench.h>
 #include <sensor_msgs/FluidPressure.h>
-#include <std_msgs/UInt16.h>
+#include <sensor_msgs/Joy.h>
+
+#include <std_msgs/Int32.h>
+#include "std_msgs/Int32MultiArray.h"
+#include <std_msgs/Float32.h>
 
 #include <eigen/Eigen/Core>
 #include <eigen/unsupported/Eigen/MatrixFunctions>
@@ -18,7 +22,6 @@ class OpenROVTeleop
 public:
     OpenROVTeleop();
 
-private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
     ros::NodeHandle nh;
@@ -26,6 +29,7 @@ private:
     int x_controllerAxis, z_controllerAxis, yaw_controllerAxis; //joy.axis indicies for axis for respective movements
     int lightsAdj, laserToggle, camTilt;   //joy.buttons indicies for ROV commands
     double x_gain, z_gain, yaw_gain;    //gain for respective movements
+    std_msgs::Int32MultiArray motor_cmds[3];       //array of motors commands [stbd, port, vert]'
 
     //published topics - future improvement: custom OpenROV msg that contains all
     ros::Publisher motorPub;
@@ -35,7 +39,9 @@ private:
 
     ros::Subscriber joySub;
 
-    double d = 0.045;       // [m] - horizontal thruster distance from centerline of CG
+    double d;       //thruster distance from center line
+
+    Eigen::Matrix3d A;
 };
 
 OpenROVTeleop::OpenROVTeleop():
@@ -44,7 +50,7 @@ OpenROVTeleop::OpenROVTeleop():
     z_controllerAxis(3),       //right stick up/down
     yaw_controllerAxis(0),      //left stick left/right
     lightsAdj(6),   // cross key left/right
-    lasertoggle(10),    //button stick right
+    laserToggle(10),    //button stick right
     camTilt(7),      // cross key up/down
     x_gain(1),
     z_gain(1),
@@ -55,7 +61,7 @@ OpenROVTeleop::OpenROVTeleop():
     nh.param("Z_stick", z_controllerAxis, z_controllerAxis);
     nh.param("Yaw_stick", yaw_controllerAxis, yaw_controllerAxis);
     nh.param("lights_adj",lightsAdj, lightsAdj);
-    nh.param("laser_tottle", lasertoggle, lasertoggle);
+    nh.param("laser_tottle", laserToggle, laserToggle);
     nh.param("camera_tilt", camTilt, camTilt);
     nh.param("x_gain", x_gain, x_gain);
     nh.param("z_gain", z_gain, z_gain);
@@ -65,34 +71,36 @@ OpenROVTeleop::OpenROVTeleop():
     //initialize publishers and subscribers
     joySub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &OpenROVTeleop::joyCallback, this);
 
-    motorPub = nh.advertise<std_msgs::Int32[3]>("/openrov/motortarget", 1);
+    motorPub = nh.advertise<std_msgs::Int32MultiArray>("/openrov/motortarget", 1);
     lightPub = nh.advertise<std_msgs::Float32>("/openrov/light_command", 1);
-    laserPub = nh.advertise<std_msgs::Int16>("/openrov/laser_toggle", 1);
-    camTiltPub = nh.advertise<std_msgs::Uint16>("/openrov/camera_servo",1);
+    laserPub = nh.advertise<std_msgs::Int32>("/openrov/laser_toggle", 1);
+    camTiltPub = nh.advertise<std_msgs::Int32>("/openrov/camera_servo",1);
 }
 
 void OpenROVTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
     double fx_d, fz_d, mz_d; //desired forces in x,z and torque about z
+    d = 0.045;       // [m] - horizontal thruster distance from centerline of CG
+
 
     // our job now is to calcualte desired prop speed, given some input.
     // in the short-term let us interpret joystick inputs as a desired wrench (force/torque)
     // instead of a twist (linear/angular velocity - more intuitive), dynamics can come later
 
-    fx_d = x_gain * joy.axis[x_controllerAxis];
-    fz_d = z_gain * joy.axis[z_controllerAxis];
-    mz_d = yaw_gain * joy.axis[yaw_controllerAxis];
+    fx_d = x_gain * joy->axes[x_controllerAxis];
+    fz_d = z_gain * joy->axes[z_controllerAxis];
+    mz_d = yaw_gain * joy->axes[yaw_controllerAxis];
 
     // thruster allocation matrix
-    Eigen::Matrix3d A << 1, 1, 0,
-                         0, 0, 1,
-                         d, -d, 0;
+    A << 1, 1, 0,
+         0, 0, 1,
+         d, -d, 0;
 
     // ROV body frame forces/torque vector [fx, fz, mz]' - marine body conventions: x: forward, y: stdb, z: down
     Eigen::Vector3d F(fx_d,fz_d,mz_d);
 
     // solve for thruster force vector [T_stbd, T_port, T_vert]'
-    Eigen::Vector3d T =  A.inverse() * F;  // A full rank <--> invertiable
+    Eigen::Vector3d T =  A.inverse() * F;  // A.inv is safe because A full rank <--> invertiable
 
     //deal with thruster saturation
 
